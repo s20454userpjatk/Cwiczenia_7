24e9774bd7cf5abd92e6772ac31fc9556e513716
s20454userpjatk <127026669+s20454userpjatk@users.noreply.github.com>
2024-06-10 11:04:13 +0200

    Solution Uploaded

---
+++ APBD-LINQ-Cwiczenia_7/LinqTutorials/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = LinqTasks.Task1();
+            //Console.WriteLine("Task 1 Results:");
+            //foreach (var emp in t)
+            //{
+            //     Console.WriteLine($"{emp.Ename} - {emp.Job}");
+            //}
+        }
+    }
+}
